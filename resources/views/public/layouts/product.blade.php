fbbe801343b4584601ed004c987aeae40e104d84
March Sevenia Putri Surga <221210053@student.mercubuana-yogya.ac.id>
2024-12-18 01:41:10 +0700

    update baru

--- resources/views/public/layouts/product.blade.php
+++ resources/views/public/layouts/product.blade.php
@@ -26,7 +26,7 @@
             <div class="product-item swiper-slide">
               <figure>
                 <a href="index.html" title="Product Title">
-                  <img src="images/chocoCrispy.jpg" alt="Product Thumbnail" class="tab-image">
+                  <img src="images/chocoCrispy1.png" alt="Product Thumbnail" class="tab-image">
                 </a>
               </figure>
               <div class="d-flex flex-column text-center">
@@ -59,11 +59,11 @@
             <div class="product-item swiper-slide">
               <figure>
                 <a href="index.html" title="Product Title">
-                  <img src="images/product-thumb-1.png" alt="Product Thumbnail" class="tab-image">
+                  <img src="images/chocoMilk.png" alt="Product Thumbnail" class="tab-image">
                 </a>
               </figure>
               <div class="d-flex flex-column text-center">
-                <h3 class="fs-6 fw-normal">Whole Wheat Sandwich Bread</h3>
+                <h3 class="fs-6 fw-normal">Choco Milk</h3>
                 <div>
                   <span class="rating">
                     <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
@@ -92,11 +92,11 @@
             <div class="product-item swiper-slide">
               <figure>
                 <a href="index.html" title="Product Title">
-                  <img src="images/product-thumb-21.png" alt="Product Thumbnail" class="tab-image">
+                  <img src="images/susKering.png" alt="Product Thumbnail" class="tab-image">
                 </a>
               </figure>
               <div class="d-flex flex-column text-center">
-                <h3 class="fs-6 fw-normal">Sunstar Fresh Melon Juice</h3>
+                <h3 class="fs-6 fw-normal">Sus Kering</h3>
                 <div>
                   <span class="rating">
                     <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
@@ -125,11 +125,11 @@
             <div class="product-item swiper-slide">
               <figure>
                 <a href="index.html" title="Product Title">
-                  <img src="images/product-thumb-22.png" alt="Product Thumbnail" class="tab-image">
+                  <img src="images/chocoCookies.png" alt="Product Thumbnail" class="tab-image">
                 </a>
               </figure>
               <div class="d-flex flex-column text-center">
-                <h3 class="fs-6 fw-normal">Gourmet Dark Chocolate</h3>
+                <h3 class="fs-6 fw-normal">Choco Cookies</h3>
                 <div>
                   <span class="rating">
                     <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
@@ -158,11 +158,11 @@
             <div class="product-item swiper-slide">
               <figure>
                 <a href="index.html" title="Product Title">
-                  <img src="images/product-thumb-23.png" alt="Product Thumbnail" class="tab-image">
+                  <img src="images/browniesChoco.png" alt="Product Thumbnail" class="tab-image">
                 </a>
               </figure>
               <div class="d-flex flex-column text-center">
-                <h3 class="fs-6 fw-normal">Sunstar Fresh Melon Juice</h3>
+                <h3 class="fs-6 fw-normal">Brownies Choco</h3>
                 <div>
                   <span class="rating">
                     <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
@@ -191,11 +191,11 @@
             <div class="product-item swiper-slide">
               <figure>
                 <a href="index.html" title="Product Title">
-                  <img src="images/product-thumb-10.png" alt="Product Thumbnail" class="tab-image">
+                  <img src="images/bananaCake.png" alt="Product Thumbnail" class="tab-image">
                 </a>
               </figure>
               <div class="d-flex flex-column text-center">
-                <h3 class="fs-6 fw-normal">Greek Style Plain Yogurt</h3>
+                <h3 class="fs-6 fw-normal">Banana Cake</h3>
                 <div>
                   <span class="rating">
                     <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
@@ -224,11 +224,11 @@
             <div class="product-item swiper-slide">
               <figure>
                 <a href="index.html" title="Product Title">
-                  <img src="images/product-thumb-11.png" alt="Product Thumbnail" class="tab-image">
+                  <img src="images/pisangCaramel.png" alt="Product Thumbnail" class="tab-image">
                 </a>
               </figure>
               <div class="d-flex flex-column text-center">
-                <h3 class="fs-6 fw-normal">Pure Squeezed No Pulp Orange Juice</h3>
+                <h3 class="fs-6 fw-normal">Pisang Caramel</h3>
                 <div>
                   <span class="rating">
                     <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
@@ -257,44 +257,11 @@
             <div class="product-item swiper-slide">
               <figure>
                 <a href="index.html" title="Product Title">
-                  <img src="images/product-thumb-12.png" alt="Product Thumbnail" class="tab-image">
+                  <img src="images/pisangNuget.png" alt="Product Thumbnail" class="tab-image">
                 </a>
               </figure>
               <div class="d-flex flex-column text-center">
-                <h3 class="fs-6 fw-normal">Fresh Oranges</h3>
-                <div>
-                  <span class="rating">
-                    <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
-                    <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
-                    <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
-                    <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
-                    <svg width="18" height="18" class="text-warning"><use xlink:href="#star-half"></use></svg>
-                  </span>
-                  <span>(222)</span>
-                </div>
-                <div class="d-flex justify-content-center align-items-center gap-2">
-                  <del>$24.00</del>
-                  <span class="text-dark fw-semibold">$18.00</span>
-                  <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary">10% OFF</span>
-                </div>
-                <div class="button-area p-3 pt-0">
-                  <div class="row g-1 mt-2">
-                    <div class="col-3"><input type="number" name="quantity" class="form-control border-dark-subtle input-number quantity" value="1"></div>
-                    <div class="col-7"><a href="#" class="btn btn-primary rounded-1 p-2 fs-7 btn-cart"><svg width="18" height="18"><use xlink:href="#cart"></use></svg> Add to Cart</a></div>
-                    <div class="col-2"><a href="#" class="btn btn-outline-dark rounded-1 p-2 fs-6"><svg width="18" height="18"><use xlink:href="#heart"></use></svg></a></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="product-item swiper-slide">
-              <figure>
-                <a href="index.html" title="Product Title">
-                  <img src="images/product-thumb-13.png" alt="Product Thumbnail" class="tab-image">
-                </a>
-              </figure>
-              <div class="d-flex flex-column text-center">
-                <h3 class="fs-6 fw-normal">Gourmet Dark Chocolate Bars</h3>
+                <h3 class="fs-6 fw-normal">Pisang Nuget</h3>
                 <div>
                   <span class="rating">
                     <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
